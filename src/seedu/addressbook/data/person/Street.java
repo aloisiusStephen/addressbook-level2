e8c0f2ebf0c6c34810094878348fefe0ddbcbaa6
Aloisius Stephen <aloisius.s@u.nus.edu>
2016-08-29 00:06:24 +0800

    Add Street class

--- src/seedu/addressbook/data/person/Street.java
+++ src/seedu/addressbook/data/person/Street.java
@@ -1,5 +1,19 @@
 package seedu.addressbook.data.person;
 
+/**
+ * Represents the Block of a Person's address the address book.
+ *
+ */
 public class Street {
+    private final String value;
+    private boolean isPrivate;
 
+    public Street(String street, boolean isPrivate) {
+        this.value = street;
+        this.isPrivate = isPrivate;
+    }
+
+    public String getStreet(){
+    return value;
+    }
 }
