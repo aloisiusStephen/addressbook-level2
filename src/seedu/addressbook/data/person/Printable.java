02431e20b465486f6c0ac87cdb683f27a05186b3
Aloisius Stephen <aloisius.s@u.nus.edu>
2016-09-08 12:31:57 +0800

    Add getPrintableString(Printable... printable) method

--- src/seedu/addressbook/data/person/Printable.java
+++ src/seedu/addressbook/data/person/Printable.java
@@ -11,18 +11,5 @@ public interface Printable {
      * Returns a printable string of each object.
      */
    public String getPrintableString();
-
-   
-   /**
-    * Returns a concatenated version of the printable strings of each object.
-    *//*
-  default String getPrintableString(Printable... printables){
-      final StringBuilder builder = new StringBuilder();
-      for(Printable p : printables) {
-      builder.append(p.getPrintableString());
-      }
-      return builder.toString();
-  }
-*/
-   
+  
 }
