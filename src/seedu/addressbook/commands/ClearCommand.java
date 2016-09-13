137f860ceeec340f69cdcfa5d8b9e0456cb1d25f
Aloisius Stephen <aloisius.s@u.nus.edu>
2016-09-13 21:46:33 +0800

    Add a polymorphic isMutating() method

--- src/seedu/addressbook/commands/ClearCommand.java
+++ src/seedu/addressbook/commands/ClearCommand.java
@@ -22,4 +22,8 @@ public class ClearCommand extends Command {
         addressBook.clear();
         return new CommandResult(MESSAGE_SUCCESS);
     }
+    
+    public boolean isMutating() {
+        return false;
+    }
 }
